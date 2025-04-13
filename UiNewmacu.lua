b1d20dbba811c3ea7cd610102f9a75466bcbbf00
BananaCatDebFK <viplambucy777@gmail.com>
2025-04-13 17:34:23 +0700

    Update UiNewmacu.lua

--- UiNewmacu.lua
+++ UiNewmacu.lua
@@ -449,7 +449,7 @@ function Andz:AddWindows(cf)
 	LogoId.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	LogoId.BorderSizePixel = 0
 	LogoId.Size = UDim2.new(1, 0, 1, 0)
-	LogoId.Image = "rbxassetid://78151437260056"
+	LogoId.Image = "rbxassetid://0"
 
 	UICorner_3.CornerRadius = UDim.new(1, 0)
 	UICorner_3.Parent = LogoId
